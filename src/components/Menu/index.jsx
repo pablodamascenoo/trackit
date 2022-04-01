b4365c037eb3f35355a4d12b6c343c3d970e367b
pablodamascenoo <pablofacex@gmail.com>
2022-03-31 21:10:43 -0300

    feat: created Menu component

---
+++ src/components/Menu/index.jsx
@@ -0,0 +1,39 @@
+import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
+import { Link } from "react-router-dom";
+
+import "react-circular-progressbar/dist/styles.css";
+
+import { Footer } from "./style";
+
+export default function Menu({ done }) {
+  return (
+    <Footer>
+      <div>
+        <Link to="/">
+          <p>Hábitos</p>
+        </Link>
+        <div>
+          <Link to="/">
+            <CircularProgressbar
+              maxValue={1}
+              background={true}
+              value={done === undefined ? 0 : done}
+              text={`Hoje`}
+              backgroundPadding={6}
+              styles={buildStyles({
+                textSize: "18px",
+                pathColor: `#fff`,
+                textColor: "#fff",
+                trailColor: "#52B6FF",
+                backgroundColor: "#52B6FF",
+              })}
+            />
+          </Link>
+        </div>
+        <Link to="/">
+          <p>Histórico</p>
+        </Link>
+      </div>
+    </Footer>
+  );
+}
---
+++ src/components/Menu/style.js
@@ -0,0 +1,32 @@
+import styled from "styled-components";
+
+export const Footer = styled.footer`
+  position: fixed;
+  bottom: 0;
+  right: 0;
+  left: 0;
+  max-height: 70px;
+  background-color: white;
+  display: flex;
+  align-items: center;
+
+  div {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    width: 100%;
+    padding: 0 36px;
+  }
+
+  a > p {
+    color: #52b6ff;
+    font-size: 18px;
+  }
+
+  div > div {
+    width: 91px;
+    height: 91px;
+    padding: 0;
+    margin-bottom: 50px;
+  }
+`;
